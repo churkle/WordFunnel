c80f671479a60b8244ab22df687ee35542ae6baa
John Doe <charfei70@gmail.com>
2018-10-25 22:25:31 -0700

    Added WordFunnelGenerator class

---
+++ WordFunnel/WordFunnelGenerator.h
@@ -0,0 +1,9 @@
+#pragma once
+// Generates Word Funnels from a file that contains a list of words separated by newlines
+
+#include <string>;
+
+class WordFunnelGenerator
+{
+	public WordFunnelGenerator(std::string filePathName);
+};
